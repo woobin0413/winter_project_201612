3dca2ba8ef16a1cf4c974146d7fd32f20b9d2947
woobin0413 <woobin0413@gmail.com>
2017-01-14 23:02:39 -0500

    delete
    delete the files

--- dbh.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost", "root", "ju0413", "imgupload");
-
-
-?>
--- login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-session_start();
-
-if(isset($_POST['submitLogin'])) {
-	$_SESSION['id'] = 1;
-	header("Location: index.php");
-}
